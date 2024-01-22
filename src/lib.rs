52e577bc2952fb1c351c67cf55058597e7bf8ff2
lcnr <rust@lcnr.de>
2024-01-22 17:18:29 +0100

    pain

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,28 @@
-/*!
-	Documentation and (de-)serialization support for LU's network protocol.
-*/
 #![feature(specialization)]
 #![allow(incomplete_features)]
-use std::io::{Write};
+use std::io::Write;
 
-use endio::{LE, Serialize};
+use endio::{Serialize, LE};
+use endio_bit::BEBitWriter;
 
-trait ReplicaS<W: Write> {}
+trait ReplicaS<W: Write> {
+    fn serialize(self);
+}
 
 impl<'a, W: Write, T> ReplicaS<W> for &'a T
 where
-	&'a T: Serialize<LE, BEBitWriter<W>>,
-{}
+    &'a T: Serialize<LE, BEBitWriter<W>>,
+{
+    default fn serialize(self) {}
+}
+
+impl<'a, W: Write> ReplicaS<W> for &'a bool {
+    fn serialize(self) {}
+}
 
 impl<W: Write, T> ReplicaS<W> for &Option<T>
 where
-	for<'a> &'a T: ReplicaS<W> + Serialize<LE, BEBitWriter<W>>,
-{}
+    for<'a> &'a T: ReplicaS<W> + Serialize<LE, BEBitWriter<W>>,
+{
+    fn serialize(self) {}
+}
