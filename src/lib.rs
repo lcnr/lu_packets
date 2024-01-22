2af3e84adda86adfff5faca09a9e30a50ca792e6
lcnr <rust@lcnr.de>
2024-01-22 17:17:10 +0100

    w

--- src/lib.rs
+++ src/lib.rs
@@ -3,40 +3,18 @@
 */
 #![feature(specialization)]
 #![allow(incomplete_features)]
-use std::io::{Result as Res, Write};
+use std::io::{Write};
 
 use endio::{LE, Serialize};
-use endio_bit::{BEBitWriter};
 
-trait ReplicaS<W: Write> {
-	fn serialize(self, writer: &mut BEBitWriter<W>) -> Res<()>;
-}
+trait ReplicaS<W: Write> {}
 
 impl<'a, W: Write, T> ReplicaS<W> for &'a T
 where
 	&'a T: Serialize<LE, BEBitWriter<W>>,
-{
-	default fn serialize(self, writer: &mut BEBitWriter<W>) -> Res<()> {
-		Serialize::serialize(self, writer)
-	}
-}
-
-
-impl<'a, W: Write> ReplicaS<W> for &'a bool {
-	fn serialize(self, writer: &mut BEBitWriter<W>) -> Res<()> {
-		writer.write_bit(*self)
-	}
-}
+{}
 
 impl<W: Write, T> ReplicaS<W> for &Option<T>
 where
 	for<'a> &'a T: ReplicaS<W> + Serialize<LE, BEBitWriter<W>>,
-{
-	fn serialize(self, writer: &mut BEBitWriter<W>) -> Res<()> {
-		writer.write_bit(self.is_some())?;
-		if let Some(x) = self {
-			ReplicaS::serialize(x, writer)?;
-		}
-		Ok(())
-	}
-}
+{}
