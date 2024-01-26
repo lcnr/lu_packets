40b281a12b09d8afe9e70fc6752b7712638c8b41
lcnr <rust@lcnr.de>
2024-01-26 10:50:00 +0100

    w

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,11 @@ use std::io::Write;
 
 use endio::{Serialize, LE};
 
-struct BitWriter<W: Write> {
+struct BitWriter<W> {
     field: W,
 }
 
-impl<W: Write> Write for BitWriter<W> {
+impl<W> Write for BitWriter<W> {
     fn write(&mut self, _: &[u8]) -> std::io::Result<usize> {
         todo!()
     }
@@ -18,20 +18,20 @@ impl<W: Write> Write for BitWriter<W> {
     }
 }
 
-trait ReplicaS<W: Write> {
+trait ReplicaS<W> {
     fn serialize(self);
 }
 
-impl<'a, W: Write, T> ReplicaS<W> for &'a T
+impl<'a, W, T> ReplicaS<W> for &'a T
 where
     &'a T: Serialize<LE, BitWriter<W>>,
 {
     default fn serialize(self) {}
 }
 
-impl<W: Write, T> ReplicaS<W> for &Option<T>
+impl<W, T> ReplicaS<W> for &Option<T>
 where
-    for<'a> &'a T: ReplicaS<W> + Serialize<LE, BitWriter<W>>,
+    for<'a> &'a T: Serialize<LE, BitWriter<W>>,
 {
     fn serialize(self) {}
 }
